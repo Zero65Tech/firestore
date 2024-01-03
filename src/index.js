1987996c6956e6794393505e1c9bb54613a2ab8b
Prashant Gupta <antshpra@gmail.com>
2024-01-03 08:30:02 +0530

    Fixes

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ exports.init = async ({ projectId, collections }) => {
 
   const Firestore = new firestore.Firestore({ projectId });
 
-  exports.Firestore = {
-    FieldValue: firestore.FieldValue,
-    Timestamp: firestore.Timestamp,
-    getAll: (...refs) => Firestore.getAll(...refs),
-    batch: () => Firestore.batch()
-  };
+  exports.FieldValue = firestore.FieldValue;
+  exports.Timestamp  = firestore.Timestamp;
+
+  exports.getAll = (...refs) => Firestore.getAll(...refs);
+  exports.batch  = () => Firestore.batch();
 
   for(let collection of collections)
-    exports.Firestore[collection] = Firestore.collection(collection);
+    exports[collection] = Firestore.collection(collection);
 
   delete exports.init;
 
